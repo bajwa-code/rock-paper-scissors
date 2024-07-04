3bb73b27d21ddaff942b72e357572369d980fa2e
rashpal <47848174+bajwa-code@users.noreply.github.com>
2024-07-04 18:37:05 +0530

    Add new function playRound()

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 function getComputerChoice() {
-    const computerChoice = ['Rock', 'Paper', 'Scissors'];
-    let randomIndex = Math.floor(Math.random()*computerChoice.length);
-    console.log(computerChoice[randomIndex]);
-    return computerChoice[randomIndex];
+    const computerChose = ['Rock', 'Paper', 'Scissors'];
+    let randomIndex = Math.floor(Math.random()*computerChose.length);
+    console.log(computerChose[randomIndex]);
+    return computerChose[randomIndex];
 }
 
 getComputerChoice();
 
 function getHumanChoice() {
-    const userChose = prompt('Enter rock, paper, or scissors:'). toLowerCase();
+    const userChose = prompt('Enter rock, paper, or scissors:').toLowerCase();
     console.log(userChose);
     return userChose;
 }
@@ -17,3 +17,18 @@ getHumanChoice();
 
 const humanScore = 0;
 const computerScore  = 0;
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        return 'It\'s a tie!'; 
+    } 
+    if ((humanChoice === 'rock' && computerChoice === 'scissors') ||
+        (humanChoice === 'paper' && computerChoice === 'rock') ||
+        (humanChoice === 'scissors' && computerChoice === 'paper')) {
+            
+        return 'You win!';
+    } else {
+        return 'Computer win!';
+    }
+
+}
