4afe5fff37756c8b2fc3c593d516d73b4774a7c9
bajwa-code <47848174+bajwa-code@users.noreply.github.com>
2025-01-07 13:19:07 +0530

    Create a new function - Function to get user input - Use if statement to verify input

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function getComputerChoice() {
+function getComputerChoice(){
     const randomValue =  Math.floor(Math.random()*3); 
     if (randomValue === 0){
         return 'rock';
@@ -8,5 +8,16 @@ function getComputerChoice() {
         return 'scissors';
     }   
 }
-
 console.log(getComputerChoice());
+
+function getHumanChoice(){
+    let choice = prompt("Enter your choice (rock, paper, or scissors): ").toLowerCase();;
+    if(choice !== 'rock' && choice !== 'paper' && choice !== 'scissors'){
+        console.log("Invalid choice. Please choose 'rock', 'paper', or 'scissors'.")
+    } else {
+        return console.log(`You chose: ${choice}`);
+    }
+    
+}
+
+getHumanChoice();
